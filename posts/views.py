7134fadc2af767502e9f4938aa77ba8ac398c5d3
angeling11 <angel-na@hotmail.es>
2021-10-22 18:15:58 -0500

    change function for createpostview

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,7 @@
 # Django
-from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 # Forms
 from posts.forms import PostForm
@@ -12,6 +11,7 @@ from posts.models import Post
 
 
 class PostFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
     template_name = 'posts/feed.html'
     model = Post
     ordering = ('-created')
@@ -20,28 +20,21 @@ class PostFeedView(LoginRequiredMixin, ListView):
 
 
 class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post details."""
     template_name = 'posts/detail.html'
     queryset = Post.objects.all()
     context_object_name = 'post'
 
 
-# Create new post
-@login_required
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create new post."""
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
 
-    return render(
-        request=request,
-        template_name='posts/new.html',
-        context={
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile,
-        }
-    )
+    def get_context_data(self, **kwargs):
+        """Add user and profile to context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
