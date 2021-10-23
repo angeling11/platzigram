f33f44366af11945d088b403784d345f1c44ad84
angeling11 <angel-na@hotmail.es>
2021-10-22 19:38:35 -0500

    change all user function to django views

--- users/views.py
+++ users/views.py
@@ -1,22 +1,24 @@
 # Django imports
+from django.contrib import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.generic import DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, FormView, UpdateView
+from django.contrib.auth import  views as auth_views
 
 # Models
 from django.contrib.auth.models import User
 from posts.models import Post
+from users.models import Profile
 
 # Forms
-from users.forms import ProfileForm, SignupForm
-
-# Views
+from users.forms import SignupForm
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
+    """User detail view."""
     template_name = 'users/detail.html'
     slug_field = 'username'
     slug_url_kwarg = 'username'
@@ -30,73 +32,40 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         return context
 
 
-# Update users's profile view
-@login_required
-def update_profile(request):
-    profile = request.user.profile
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
+class LoginView(auth_views.LoginView):
+    """Login view."""
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
 
-        if form.is_valid():
-            data = form.cleaned_data
 
-            profile.website = data['website']
-            profile.phone_number = data['phone_number']
-            profile.biography = data['biography']
-            profile.picture = data['picture']
-            profile.save()
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
+    pass
 
-            url = reverse('users:detail', kwargs={'username': request.user.username})
-            return redirect(url)
-    else:
-        form = ProfileForm()
+
+class UpdateProfileView(LoginRequiredMixin, UpdateView):
+    """Update profile view."""
+    template_name = 'users/update_profile.html'
+    model = Profile
+    fields = ['website', 'biography', 'phone_number', 'picture']
+
+    def get_object(self):
+        """Return user's profile."""
+        return self.request.user.profile
     
-    return render(
-        request=request,
-        template_name='users/update_profile.html',
-        context={
-            'profile': profile,
-            'user': request.user,
-            'form': form,
-        }
-    )
+    def get_success_url(self):
+        """Return to user's profile."""
+        username = self.object.user.username
+        return reverse('users:detail', kwargs={'username': username})
 
 
-# User login view
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username or password.'})
-    return render(request, 'users/login.html')
+class SignupView(FormView):
+    """Users signup view."""
+    template_name = 'users/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('users:login')
 
-
-# User signup view
-def signup_view(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect('users:login')
-    else:
-        form = SignupForm()
-
-    return render(
-        request=request,
-        template_name='users/signup.html',
-        context={'form': form}
-    )
-
-
-# User logout view
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect('users:login')
+    def form_valid(self, form):
+        """Save form data."""
+        form.save()
+        return super().form_valid(form)
